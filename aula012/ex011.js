bd35d9dba26e27f1b70ffe41f0ebe234f2207528
Gustavo Ferreira <gustavohenriquelimaa@hotmail.com>
2026-03-02 22:46:40 -0300

    atlz nova

---
+++ aula012/ex011.js
@@ -0,0 +1,9 @@
+var idade = 22
+console.log(`Voê tem ${idade} anos.`)
+if(idade < 16){
+    console.log("Menor de idade")
+} else if(idade < 18 || idade > 65){
+    console.log("Maior de idade")
+} else {
+    console.log("Voto obrigatório")
+}
