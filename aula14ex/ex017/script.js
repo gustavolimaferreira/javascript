32017401f7a892e39675b603894b4e493df18104
Gustavo Ferreira <gustavohenriquelimaa@hotmail.com>
2026-04-01 20:17:44 -0300

    atlz ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,4 +1,23 @@
 function gerar() {
+    let num = document.getElementById('num')
+    let  tab = document.getElementById('lista')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = '' // Limpa a lista antes de gerar a nova tabuada
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text  = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+/*
+function gerar() {
     var num = Number(document.getElementById('num').value)
     var res = document.getElementById('res')
     var tab = document.getElementById('lista')
@@ -15,3 +34,4 @@ function gerar() {
         }        
     }
 }
+*/
